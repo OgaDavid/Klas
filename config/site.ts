596e7970aeac52bf6fcb5c43c259a6612398987a
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-10-06 10:33:47 -1100

    chore(app): add dependencies, github workflow semantic versioning

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+    name: "Klas",
+    url: "https://tryklas.vercel.app",
+    ogImage: "../app/opengraph-image.jpg",
+    description:
+      "Free Online Teaching Tool: Live Classes, Community | Klas.",
+    links: {
+      twitter: "https://twitter.com/klas_hq",
+      instagram: "https://www.instagram.com/tryklas/",
+      linkedin: "https://www.linkedin.com/company/tryklas",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
