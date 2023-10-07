fe608b3107707df169937767ee9a33bc4d433d01
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-10-06 13:25:30 -1100

    refactor(default): add font, tailwind merge and clsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import { siteConfig } from "@/config/site";
 
-const inter = Inter({ subsets: ["latin"] });
+const dm_sans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
@@ -67,7 +67,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={dm_sans.className}>{children}</body>
     </html>
   );
 }
