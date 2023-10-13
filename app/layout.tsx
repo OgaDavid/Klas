129def2052c05643b5e93f9fe15a7ec87ef7ba1c
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-10-12 18:01:26 -1100

    chore: fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,3 +72,17 @@ export default function RootLayout({
     </html>
   );
 }
+
+// <!--Start of Tawk.to Script-->
+// <script type="text/javascript">
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/6528cc966fcfe87d54b95aa0/1hcjlubgk';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+// </script>
+// <!--End of Tawk.to Script-->
