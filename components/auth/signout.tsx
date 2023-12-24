ea2cf57f9142c611d11de98f5aa55c9a3445d339
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-12-24 18:51:22 +0100

    chore: fix site config url

--- components/auth/signout.tsx
+++ components/auth/signout.tsx
@@ -2,12 +2,13 @@
 import { signOut } from "next-auth/react";
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
 
 const SignOutButton = () => {
   return (
     <Button
       variant="secondary"
-      onClick={() => signOut({ callbackUrl: "http://localhost:3000/login" })}
+      onClick={() => signOut({ callbackUrl: `${siteConfig.url}/login` })}
     >
       Sign Out
     </Button>
--- config/site.ts
+++ config/site.ts
@@ -1,14 +1,16 @@
 export const siteConfig = {
-    name: "Klas",
-    url: "https://tryklas.vercel.app",
-    ogImage: "../app/opengraph-image.jpg",
-    description:
-      "Free Online Teaching Tool: Live Classes, Community | Klas.",
-    links: {
-      twitter: "https://twitter.com/klas_hq",
-      instagram: "https://www.instagram.com/tryklas/",
-      linkedin: "https://www.linkedin.com/company/tryklas",
-    },
-  }
-  
-  export type SiteConfig = typeof siteConfig
+  name: "Klas",
+  url:
+    process.env.NODE_ENV !== "development"
+      ? "https://tryklas.vercel.app"
+      : "http://localhost:3000",
+  ogImage: "../app/opengraph-image.jpg",
+  description: "Free Online Teaching Tool: Live Classes, Community | Klas.",
+  links: {
+    twitter: "https://twitter.com/klas_hq",
+    instagram: "https://www.instagram.com/tryklas/",
+    linkedin: "https://www.linkedin.com/company/tryklas",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
