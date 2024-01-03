a9b2ab73f91e4c5c50b03b236da8cbcefdde21ca
OgaDavid <oluwademiladedavidoga@gmail.com>
2024-01-03 18:08:25 +0100

    chore(prisma): add course, lesson, faq schemas 🚀

--- app/(routes)/(dashboard)/dashboard/create/_components/create-lesson-form.tsx
+++ app/(routes)/(dashboard)/dashboard/create/_components/create-lesson-form.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CreateLessonForm = () => {
   return (
-    <div className='mt-[100px] mx-auto max-w-[943px]'>CreateLessonForm</div>
+    <div className='mt-[100px] mx-auto max-w-[943px] flex items-center justify-center'>CreateLessonForm</div>
   )
 }
 
